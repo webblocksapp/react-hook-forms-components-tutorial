55ed11d9e6178848d59b857f7b5bf980b6648562
Mauricio Rivera <mauriver21@gmail.com>
2022-11-10 16:07:13 -0500

    Refactor with use controller

--- src/components/TextField/index.stories.tsx
+++ src/components/TextField/index.stories.tsx
@@ -1,7 +1,9 @@
 import { TextField } from '@components';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
+
+type Schema = { name: string };
 
 const schema: yup.SchemaOf<{ name: string }> = yup.object({
   name: yup.string().required(),
@@ -13,23 +15,40 @@ export default {
 };
 
 export const Primary = () => {
-  const { control } = useForm({
+  const { control } = useForm<Schema>({
     resolver: yupResolver(schema),
     mode: 'all',
   });
 
   return (
     <>
-      <TextField
-        label="Name"
-        name="name"
-        control={control}
-        onChange={() => {
-          console.log('hello world');
-        }}
-      />
-
-      <TextField label="Any Field" />
+      <div>
+        <TextField
+          label="Name"
+          name="name"
+          control={control}
+          onChange={() => {
+            console.log('hello world');
+          }}
+          onBlur={() => {
+            console.log('hello world on blur');
+          }}
+          value="Hello"
+          helperText="Write a valid name"
+        />
+      </div>
+      <div>
+        <TextField
+          label="Any Field"
+          onChange={() => {
+            console.log('hello world');
+          }}
+          onBlur={() => {
+            console.log('hello world on blur');
+          }}
+          value="xxxxx"
+        />
+      </div>
     </>
   );
 };
--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,29 +1,35 @@
 import { TextField as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@mui/material';
-import { Control, Controller } from 'react-hook-form';
+import { Control, Controller, useController } from 'react-hook-form';
 
-export type TextFieldProps = MuiTextFieldProps & { control?: Control };
+export type TextFieldProps = MuiTextFieldProps & { control?: Control<any> };
 
 export const TextField: React.FC<TextFieldProps> = ({ control, ...rest }) => {
-  const onChange: TextFieldProps['onChange'] = (event) => {
-    //Connecting it with react hook forms
+  const controller =
+    control &&
+    useController({
+      name: rest.name || '',
+      control,
+      defaultValue: rest.value,
+    });
 
+  const onChange: TextFieldProps['onChange'] = (event) => {
+    controller?.field?.onChange(event);
     rest?.onChange?.(event);
   };
 
+  const onBlur: TextFieldProps['onBlur'] = (event) => {
+    controller?.field?.onBlur();
+    rest?.onBlur?.(event);
+  };
+
   return (
-    <Controller
-      control={control}
-      name={rest.name || ''}
-      render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
-        <MuiTextField
-          {...rest}
-          onChange={onChange}
-          onBlur={onBlur}
-          value={value || ''}
-          helperText={error?.message}
-          error={Boolean(error?.message)}
-        />
-      )}
+    <MuiTextField
+      {...rest}
+      onChange={onChange}
+      onBlur={onBlur}
+      value={controller?.field?.value || rest.value}
+      helperText={controller?.fieldState?.error?.message || rest.helperText}
+      error={Boolean(controller?.fieldState?.error?.message) || rest.error}
     />
   );
 };
