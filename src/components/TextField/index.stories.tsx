380bd960f096d90fec585985e39b0c745480100e
Mauricio Rivera <mauriver21@gmail.com>
2022-11-10 12:36:36 -0500

    Added Controller code abstraction

--- src/components/TextField/index.stories.tsx
+++ src/components/TextField/index.stories.tsx
@@ -1,4 +1,11 @@
 import { TextField } from '@components';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
+const schema: yup.SchemaOf<{ name: string }> = yup.object({
+  name: yup.string().required(),
+});
 
 export default {
   title: 'Form Components/TextField',
@@ -6,13 +13,23 @@ export default {
 };
 
 export const Primary = () => {
+  const { control } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'all',
+  });
+
   return (
-    <TextField
-      label="Name"
-      name="name"
-      onChange={() => {
-        console.log('Hello universe');
-      }}
-    />
+    <>
+      <TextField
+        label="Name"
+        name="name"
+        control={control}
+        onChange={() => {
+          console.log('hello world');
+        }}
+      />
+
+      <TextField label="Any Field" />
+    </>
   );
 };
--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -1,13 +1,29 @@
 import { TextField as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@mui/material';
+import { Control, Controller } from 'react-hook-form';
 
-export type TextFieldProps = MuiTextFieldProps;
+export type TextFieldProps = MuiTextFieldProps & { control?: Control };
 
-export const TextField: React.FC<MuiTextFieldProps> = (props) => {
+export const TextField: React.FC<TextFieldProps> = ({ control, ...rest }) => {
   const onChange: TextFieldProps['onChange'] = (event) => {
     //Connecting it with react hook forms
 
-    props?.onChange?.(event);
+    rest?.onChange?.(event);
   };
 
-  return <MuiTextField {...props} onChange={onChange} />;
+  return (
+    <Controller
+      control={control}
+      name={rest.name || ''}
+      render={({ field: { onChange, onBlur, value }, fieldState: { error } }) => (
+        <MuiTextField
+          {...rest}
+          onChange={onChange}
+          onBlur={onBlur}
+          value={value || ''}
+          helperText={error?.message}
+          error={Boolean(error?.message)}
+        />
+      )}
+    />
+  );
 };
